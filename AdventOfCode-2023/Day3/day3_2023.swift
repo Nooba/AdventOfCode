cd47f25fd62e715cb1ca2ecd90e637abb86a7ec4
Edouard Siegel <edouard.siegel@fabernovel.com>
2023-12-05 14:50:19 +0100

    AoC2023 - Day 3 - Part 2

--- AdventOfCode-2023/Day3/day3_2023.swift
+++ AdventOfCode-2023/Day3/day3_2023.swift
@@ -11,13 +11,24 @@ private class Cell {
     let value: String
     var isValid: Bool
 
-    init(value: String, isValid: Bool) {
+    init(value: String, isValid: Bool, adjacentNumbers: [Int]) {
         self.value = value
         self.isValid = isValid
+        self.adjacentNumbers = adjacentNumbers
     }
 
     var isDigit: Bool {
         return Int(value) != nil
+    }
+
+    var isPotentialGear: Bool {
+        return value == "*"
+    }
+    var adjacentNumbers: [Int]
+    var isGear: Bool { isPotentialGear && adjacentNumbers.count == 2 }
+    var gearPower: Int {
+        guard !adjacentNumbers.isEmpty else { return 0 }
+        return adjacentNumbers.reduce(1, *)
     }
 }
 
@@ -48,6 +59,21 @@ private struct Map {
             print(validNumbers)
             return validNumbers
         }
+    }
+
+    func numberAt(x: Int, y: Int) -> Int? {
+        var currentMinX = x
+        var currentMaxX = x
+        while let previous = self[currentMinX - 1, y], previous.isDigit {
+            currentMinX -= 1
+        }
+        while let next = self[currentMaxX + 1, y], next.isDigit {
+            currentMaxX += 1
+        }
+        let intString = (currentMinX...currentMaxX).map { x in
+            self[x, y]!.value
+        }.joined()
+        return Int(intString)
     }
 }
 
@@ -91,7 +117,7 @@ private func parseLines(_ lines: [String]) throws -> Map {
     let rows = lines.map { line in
         line.map { cell in
             let string = String(cell)
-            return Cell(value: string, isValid: string.isValid)
+            return Cell(value: string, isValid: string.isValid, adjacentNumbers: [])
         }
     }
     return Map(rows: rows)
@@ -105,12 +131,63 @@ func day3_2023_A() throws -> Int {
     return flat.reduce(0, +)
 }
 
-private func parseLineSecondPart(_ line: String) throws -> Int {
-    return -1
+
+private func processSecondPart(_ map: Map) {
+    (0...(map.maxY)).forEach { y in
+        (0...(map.maxX)).forEach { x in
+            if let cell = map[x, y], cell.isPotentialGear {
+                updatePotentialGearCell(map, x: x, y: y)
+            }
+        }
+    }
+}
+
+private func updatePotentialGearCell(_ map: Map, x: Int, y: Int) {
+    var adjacentDigits = [
+        map[x-1, y],
+        map[x+1, y],
+    ]
+    if let top = map[x, y-1], top.isDigit {
+        adjacentDigits.append(top)
+    } else {
+        adjacentDigits.append(map[x-1, y-1])
+        adjacentDigits.append(map[x+1, y-1])
+    }
+    if let top = map[x, y+1], top.isDigit {
+        adjacentDigits.append(top)
+    } else {
+        adjacentDigits.append(map[x-1, y+1])
+        adjacentDigits.append(map[x+1, y+1])
+    }
+    guard adjacentDigits.compactMap({ $0 }).filter({ $0.isDigit }).count == 2 else {
+        return
+    }
+
+    var adjacentNumbers = [
+        map.numberAt(x: x-1, y: y),
+        map.numberAt(x: x+1, y: y)
+    ]
+    if let top = map[x, y-1], top.isDigit {
+        adjacentNumbers.append(map.numberAt(x: x, y: y-1))
+    } else {
+        adjacentNumbers.append(map.numberAt(x: x-1, y: y-1))
+        adjacentNumbers.append(map.numberAt(x: x+1, y: y-1))
+    }
+    if let top = map[x, y+1], top.isDigit {
+        adjacentNumbers.append(map.numberAt(x: x, y: y+1))
+    } else {
+        adjacentNumbers.append(map.numberAt(x: x-1, y: y+1))
+        adjacentNumbers.append(map.numberAt(x: x+1, y: y+1))
+    }
+
+    let adjacent = adjacentNumbers.compactMap { $0 }
+    map[x, y]?.adjacentNumbers = adjacent
 }
 
 func day3_2023_B() throws -> Int {
-    let lines = try FileReader(filename: "day3_2023_example").getLines()
-    let numbers = try lines.map(parseLineSecondPart(_:))
-    return numbers.reduce(0, +)
+    let lines = try FileReader(filename: "day3_2023_input").getLines()
+    let map = try parseLines(lines)
+    processSecondPart(map)
+    let flat = map.rows.flatMap { $0.map { $0.gearPower } }
+    return flat.reduce(0, +)
 }
