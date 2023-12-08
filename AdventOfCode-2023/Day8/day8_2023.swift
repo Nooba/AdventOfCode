aaaae695117cd9c73a864c8d7950ef965358a639
Edouard Siegel <edouard.siegel@fabernovel.com>
2023-12-08 11:37:16 +0100

    AoC2023 - Day 8 - Part 2

--- AdventOfCode-2023/Day8/day8_2023.swift
+++ AdventOfCode-2023/Day8/day8_2023.swift
@@ -14,7 +14,7 @@ private enum Direction: String {
     case right = "R"
 }
 
-private struct Node: CustomStringConvertible {
+private struct Branch: CustomStringConvertible {
     let left: String
     let right: String
 
@@ -36,17 +36,17 @@ private func parseInstructions(_ line: String) -> [Direction] {
     return line.map { Direction(rawValue: String($0))! }
 }
 
-private func parseMap(_ lines: [String]) throws -> [String: Node] {
-    var map: [String: Node] = [:]
+private func parseMap(_ lines: [String]) throws -> [String: Branch] {
+    var map: [String: Branch] = [:]
     try lines.forEach { line in
-        try parseRegexp(line, capturePattern: #"(\D{3}) = \((\D{3}), (\D{3})\)"#) { groups in
-            map[groups[0]] = Node(left: groups[1], right: groups[2])
+        try parseRegexp(line, capturePattern: #"(\S{3}) = \((\S{3}), (\S{3})\)"#) { groups in
+            map[groups[0]] = Branch(left: groups[1], right: groups[2])
         }
     }
     return map
 }
 
-private func process(_ map: [String: Node], instructions: [Direction]) -> Int {
+private func process(_ map: [String: Branch], instructions: [Direction]) -> Int {
     var currentPos = "AAA"
     var counter = 0
     while currentPos != "ZZZ" {
@@ -67,12 +67,45 @@ func day8_2023_A() throws -> Int {
 
 // MARK: - Part 2
 
-private func parseLineSecondPart(_ line: String) throws -> Int {
-    return -1
+private struct Position {
+    let nodes: [String]
+
+    var isFinished: Bool {
+        return nodes.allSatisfy { $0.hasSuffix("Z") }
+    }
 }
 
+private func parseStartingPosition(nodes: [String]) -> Position {
+    return Position(nodes: nodes.filter { $0.hasSuffix("A") })
+}
+
+private func process(startingPosition: Position, map: [String: Branch], instructions: [Direction]) -> Int {
+    var currentPos = startingPosition
+    var counter = 0
+    while !currentPos.isFinished {
+        print(currentPos.nodes)
+        let instruction = instructions[counter % instructions.count]
+        let mappedNodes = currentPos.nodes.map { node in
+            return map[node]![instruction]
+        }
+        currentPos = Position(nodes: mappedNodes)
+        counter += 1
+    }
+    return counter
+}
+
+
 func day8_2023_B() throws -> Int {
-    let lines = try FileReader(filename: "day8_2023_input").getLines()
-    let numbers = try lines.map(parseLineSecondPart(_:))
-    return numbers.reduce(0, +)
+    let groupedLines = try FileReader(filename: "day8_2023_input").getGroupedLines()
+    let instructions = parseInstructions(groupedLines[0].first!)
+    let mapStrings = groupedLines[1]
+    let map = try parseMap(mapStrings)
+    let positions = parseStartingPosition(nodes: Array(map.keys))
+    let loops = positions.nodes.map { startingNode in
+        process(startingPosition: Position(nodes: [startingNode]), map: map, instructions: instructions)
+    }
+    // Bruteforcing by processing the loops at the same time is too long, the example / definition of the problem shows
+    // a cycle between the example loops. Thus the idea that it must be the Least Common Multiple between each loops
+    // cycle
+    return lcm(loops)
 }
---
+++ Helpers/Maths.swift
@@ -0,0 +1,32 @@
+//
+//  Maths.swift
+//  AdventOfCode
+//
+//  Created by Edouard Siegel on 08/12/2023.
+//
+
+import Foundation
+
+// GCD of two numbers:
+func gcd(_ a: Int, _ b: Int) -> Int {
+    var (a, b) = (a, b)
+    while b != 0 {
+        (a, b) = (b, a % b)
+    }
+    return abs(a)
+}
+
+// GCD of a vector of numbers:
+func gcd(_ vector: [Int]) -> Int {
+    return vector.reduce(0, gcd)
+}
+
+// LCM of two numbers:
+func lcm(a: Int, b: Int) -> Int {
+    return (a / gcd(a, b)) * b
+}
+
+// LCM of a vector of numbers:
+func lcm(_ vector : [Int]) -> Int {
+    return vector.reduce(1, lcm)
+}
