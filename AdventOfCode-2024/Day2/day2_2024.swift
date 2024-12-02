133bbd4c9b967d9790a5a44b569e6b03a3b36477
Edouard Siegel <edouard@beiningbogen.no>
2024-12-02 10:46:10 +0100

    2024 Day 2 - Part B

--- AdventOfCode-2024/Day2/day2_2024.swift
+++ AdventOfCode-2024/Day2/day2_2024.swift
@@ -52,7 +52,57 @@ func day2_2024_A() throws -> Int {
     return mapped.filter { $0 }.count
 }
 
+// MARK: - Part B
+
 func day2_2024_B() throws -> Int {
     let lines = try FileReader(filename: "day2_2024_input").getLines()
-    return -1
+    let reports = try lines.map(parseLine(_:))
+    let unsafe = reports.filter { !isSafe(levels: $0) }
+    let superUnsafe = unsafe.filter { unsafeReport in
+        let length = unsafeReport.count
+        var dampenedReports: [[Int]] = []
+        for i in (0..<length) {
+            var copy = unsafeReport
+            copy.remove(at: i)
+            dampenedReports.append(copy)
+        }
+        return !dampenedReports.contains { isSafe(levels: $0) }
+    }
+    return reports.count - superUnsafe.count
+}
+
+private func isSafe(levels: [Int]) -> Bool {
+    var positiveSlope: Bool?
+    var previous: Int!
+    var isSafe = true
+    print(levels)
+    levels.enumerated().forEach { (index, element) in
+        if !isSafe { return }
+        if index == 0 {
+            previous = element
+            return
+        }
+        print(element)
+        if index == 1 {
+            positiveSlope = element >= previous
+        }
+        let abs = abs(element - previous)
+        if abs > 3 {
+            print("distance")
+            isSafe = false
+            return
+        }
+        if abs == 0 {
+            print("flat slope")
+            isSafe = false
+            return
+        }
+        if (element >= previous) != positiveSlope {
+            print("slope change")
+            isSafe = false
+            return
+        }
+        previous = element
+    }
+    return isSafe
 }
