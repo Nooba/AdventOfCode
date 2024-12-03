5b3ff14b9949c5bee4839e309f143d4968c4a119
Edouard Siegel <edouard@beiningbogen.no>
2024-12-03 11:07:25 +0100

    2024 Day 3 - Part B

--- AdventOfCode-2024/Day3/day3_2024.swift
+++ AdventOfCode-2024/Day3/day3_2024.swift
@@ -25,8 +25,32 @@ func day3_2024_A() throws -> Int {
 
 // MARK: - Part B
 
+var enabled = true
+
+private func parseLinePartB(_ line: String) throws -> Int {
+    var result = 0
+    try parseRegexp(line, capturePattern: #"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"#, getAllMatches: true) { groups in
+        let mapped = groups.compactMap { group -> String? in
+            if group == "do()" {
+                enabled = true
+                return nil
+            } else if group == "don't()" {
+                enabled = false
+                return nil
+            } else if enabled {
+                return group.replacingOccurrences(of: "mul(", with: "").replacingOccurrences(of: ")", with: "")
+            }
+            return nil
+        }
+        let components = mapped.map { $0.components(separatedBy: ",") }
+        let values = components.map { $0.map { Int($0)! }.reduce(1, *) }
+        result += values.reduce(0, +)
+    }
+    return result
+}
+
+
 func day3_2024_B() throws -> Int {
-    let lines = try FileReader(filename: "day3_2024_example").getLines()
-    let reports = try lines.map(parseLine(_:))
-    return -1
+    let lines = try FileReader(filename: "day3_2024_input").getLines()
+    return try lines.map { try parseLinePartB($0) }.reduce(0, +)
 }
