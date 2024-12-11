6eb3fe50166009cce94fc7c7af19e9619be8b4cd
Edouard Siegel <edouard@beiningbogen.no>
2024-12-11 13:15:16 +0100

    2024 Day 11 - Part B

--- AdventOfCode-2024/Day11/day11_2024.swift
+++ AdventOfCode-2024/Day11/day11_2024.swift
@@ -37,7 +37,7 @@ private func parseLine(_ line: String) -> Stone {
 private func blinkAllStones(startingAt stone: Stone?) {
     var currentStone: Stone? = stone
     while currentStone != nil {
-        var nextStone: Stone? = currentStone!.nextStone
+        let nextStone: Stone? = currentStone!.nextStone
         blink(stone: currentStone)
         currentStone = nextStone
     }
@@ -50,11 +50,19 @@ private func blink(stone: Stone?) {
         stone.value += 1
         return
     }
+    if let (leftValue, rightValue) = memoization[stone.value] {
+        let rightStone = Stone(value: rightValue, nextStone: nextStone)
+        stone.value = leftValue
+        stone.nextStone = rightStone
+        return
+    }
     let intString = String(stone.value)
     if intString.count.isMultiple(of: 2) {
         let (leftString, rightString) = intString.splitStringInHalf()
-        let rightStone = Stone(value: Int(rightString)!, nextStone: nextStone)
-        stone.value = Int(leftString)!
+        let (leftValue, rightValue) = (Int(leftString)!, Int(rightString)!)
+        memoization[stone.value] = (leftValue, rightValue)
+        let rightStone = Stone(value: rightValue, nextStone: nextStone)
+        stone.value = leftValue
         stone.nextStone = rightStone
         return
     }
@@ -92,21 +100,72 @@ func day11_2024_A() throws -> Int {
     for _ in 0..<amountOfBlink {
         blinkAllStones(startingAt: stone)
     }
+    var buckets: [Int: Int] = [:]
+    var currentStone: Stone? = stone
+    while currentStone != nil {
+        buckets[currentStone!.value] = (buckets[currentStone!.value] ?? 0) + 1
+        currentStone = currentStone!.nextStone
+    }
     return countStones(stone)
 }
 
 // MARK: - Part B
 
+private var memoization: [Int: (Int, Int)] = [:]
+
+private func parseLineB(_ line: String) -> [Int] {
+    return line.components(separatedBy: .whitespacesAndNewlines).map { string in
+        Int(string)!
+    }
+}
+
+private func blinkBuckets() {
+    var nextBuckets: [Int: Int] = [:]
+    buckets.forEach { (key: Int, value: Int) in
+        if key == 0 {
+            nextBuckets[1] = (nextBuckets[1] ?? 0) + value
+            return
+        }
+        if let (leftKey, rightKey) = memoization[key] {
+            let leftValue = nextBuckets[leftKey] ?? 0
+            nextBuckets[leftKey] = leftValue + value
+            // !!! leftKey can be the same as rightKey. reread the value after inserting left.
+            let rightValue = nextBuckets[rightKey] ?? 0
+            nextBuckets[rightKey] = rightValue + value
+            return
+        }
+        let intString = String(key)
+        if intString.count.isMultiple(of: 2) {
+            let (leftString, rightString) = intString.splitStringInHalf()
+            let (leftKey, rightKey) = (Int(leftString)!, Int(rightString)!)
+            let leftValue = nextBuckets[leftKey] ?? 0
+            nextBuckets[leftKey] = leftValue + value
+            // !!! leftKey can be the same as rightKey. reread the value after inserting left.
+            let rightValue = nextBuckets[rightKey] ?? 0
+            nextBuckets[rightKey] = rightValue + value
+            memoization[key] = (leftKey, rightKey)
+            return
+        }
+        nextBuckets[key * 2024] = (nextBuckets[key * 2024] ?? 0) + value
+    }
+    buckets = nextBuckets
+}
+
+private var buckets: [Int: Int] = [:]
+
 func day11_2024_B() throws -> Int {
     let lines = try FileReader(filename: "day11_2024_input").getLines()
-    let stone = parseLine(lines[0])
+    let values = parseLineB(lines[0])
+    values.forEach { buckets[$0] = (buckets[$0] ?? 0) + 1 }
     let amountOfBlink = 75
     let start = Date()
     for i in 0..<amountOfBlink {
+        blinkBuckets()
         let finish = Date()
         let durationString = String(format: "%.3f", start.distance(to: finish))
-        print("\(i), computed in \(durationString) seconds")
-        blinkAllStones(startingAt: stone)
     }
-    return countStones(stone)
+    _ = try day11_2024_A()
+    return buckets.values.reduce(0, +)
+
+
 }
