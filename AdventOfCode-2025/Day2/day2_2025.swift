670fe84820c29e3b6c94dfddb07991531a02971f
Edouard Siegel <edouard@beiningbogen.no>
2025-12-02 10:17:00 +0100

    Day 2 - Part A

--- AdventOfCode-2025/Day2/day2_2025.swift
+++ AdventOfCode-2025/Day2/day2_2025.swift
@@ -7,49 +7,45 @@
 
 import Foundation
 
+private extension Int {
+    var isValid: Bool {
+        let string = String(self)
+        guard string.count.isMultiple(of: 2) else {
+            return false
+        }
+        let mid = string.count / 2
+        let startIndex = string.startIndex
+        let middleIndex = string.index(startIndex, offsetBy: mid)
+        let endIndex = string.endIndex
+        let firstHalf = string[startIndex..<middleIndex]
+        let secondHalf = string[middleIndex..<endIndex]
+        return firstHalf == secondHalf
+    }
+}
+
 private func parseLine(_ line: String) throws -> [Int] {
     return (line.components(separatedBy: .whitespacesAndNewlines)).compactMap { Int($0) }
 }
 
-func day2_2025_A() throws -> Int {
-    let lines = try FileReader(filename: "day2_2025_input").getLines()
-    let numbers = try lines.map(parseLine(_:))
-    let mapped = numbers.map { line -> Bool in
-        var positiveSlope: Bool?
-        var previous: Int!
-        var isSafe = true
-        print(line)
-        line.enumerated().forEach { (index, element) in
-            if !isSafe { return }
-            if index == 0 {
-                previous = element
-                return
-            }
-            print(element)
-            if index == 1 {
-                positiveSlope = element >= previous
-            }
-            let abs = abs(element - previous)
-            if abs > 3 {
-                print("distance")
-                isSafe = false
-                return
-            }
-            if abs == 0 {
-                print("flat slope")
-                isSafe = false
-                return
-            }
-            if (element >= previous) != positiveSlope {
-                print("slope change")
-                isSafe = false
-                return
-            }
-            previous = element
-        }
-        return isSafe
+private struct Tuple {
+    let start: Int
+    let end: Int
+
+    init(string: String) {
+        let comps = string.components(separatedBy: "-").map { Int($0) }
+        start = comps[0]!
+        end = comps[1]!
     }
-    return mapped.filter { $0 }.count
+
+    var invalidIds: [Int] {
+        Array(start...end).filter(\.isValid)
+    }
+}
+
+func day2_2025_A() throws -> Int {
+    let line = try FileReader(filename: "day2_2025_input").getLines()[0]
+    let tuples = line.components(separatedBy: ",").map { Tuple(string: $0) }
+    return tuples.flatMap { $0.invalidIds }.reduce(0, +)
 }
 
 // MARK: - Part B
