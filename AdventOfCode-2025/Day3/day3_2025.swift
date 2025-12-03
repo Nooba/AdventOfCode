1877232052271119fbc0c8aee9817bb63c0f118e
Edouard Siegel <edouard@beiningbogen.no>
2025-12-03 10:35:31 +0100

    Day 3 - Part A

--- AdventOfCode-2025/Day3/day3_2025.swift
+++ AdventOfCode-2025/Day3/day3_2025.swift
@@ -7,20 +7,27 @@
 
 import Foundation
 
-private func parseLine(_ line: String) throws -> Int {
-    var result = 0
-    try parseRegexp(line, capturePattern: #"mul\(\d{1,3},\d{1,3}\)"#, getAllMatches: true) { groups in
-        let mapped = groups.map { $0.replacingOccurrences(of: "mul(", with: "").replacingOccurrences(of: ")", with: "") }
-        let components = mapped.map { $0.components(separatedBy: ",") }
-        let values = components.map { $0.map { Int($0)! }.reduce(1, *) }
-        result += values.reduce(0, +)
-    }
-    return result
-}
-
 func day3_2025_A() throws -> Int {
     let lines = try FileReader(filename: "day3_2025_input").getLines()
-    return try lines.map { try parseLine($0) }.reduce(0, +)
+    let results = lines.map { line in
+        var first = 0
+        var second = 0
+        line.forEach { char in
+            let current = Int(String(char))!
+            if current >= second {
+                if first < second {
+                    first = second
+                }
+                second = current
+            } else if second > first {
+                first = second
+                second = current
+            }
+        }
+        return first * 10 + second
+    }
+//    print(results)
+    return results.reduce(0, +)
 }
 
 // MARK: - Part B
