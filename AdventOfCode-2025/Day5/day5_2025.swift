4aa099e1787eb5d0886adf1ab9a10751fd0cc51c
Edouard Siegel <edouard@beiningbogen.no>
2025-12-05 11:03:08 +0100

    Day 5

--- AdventOfCode-2025/Day5/day5_2025.swift
+++ AdventOfCode-2025/Day5/day5_2025.swift
@@ -7,78 +7,112 @@
 
 import Foundation
 
-private func parseDictionaryLine(_ line: String) throws -> Pair {
-    let components = line.components(separatedBy: "|").map { Int($0)! }
-    return Pair(first: components[0], last: components[1])
-}
-
-private func testUpdate(line: String, rules: Set<Pair>) -> Int {
-    let pages = line.components(separatedBy: ",").map { Int($0)! }
-    var printedPages = Set<Int>()
-    var isGood = true
-    pages.forEach { page in
-        guard isGood else { return }
-        let rulesToApply = rules.filter { $0.last == page }
-        if rulesToApply.count == 0 {
-            printedPages.insert(page)
-            return
-        }
-        if (rulesToApply.allSatisfy { pair in
-            if pages.contains(pair.first) {
-                return printedPages.contains(pair.first)
-            }
-            return true
-        }) {
-            printedPages.insert(page)
-            return
-        }
-        isGood = false
-    }
-    if isGood {
-        return pages[pages.count / 2]
-    }
-    return 0
-}
-
-private struct Pair: Hashable {
-    let first: Int
-    let last: Int
-}
-
 func day5_2025_A() throws -> Int {
     let lines = try FileReader(filename: "day5_2025_input").getGroupedLines()
-    let pairs = try lines[0].map { try parseDictionaryLine($0)}
-    let rules = Set<Pair>(pairs)
-    let results = lines[1].map { testUpdate(line: $0, rules: rules) }
-    return results.reduce(0, +)
+    let freshRanges = lines[0].map {
+        let components = $0.components(separatedBy: "-")
+        return Int(components[0])!...Int(components[1])!
+    }
+    let result = lines[1].reduce(into: 0) { partialResult, element in
+        let id = Int(element)!
+        if freshRanges.first(where: { $0.contains(id) }) != nil {
+            partialResult += 1
+        }
+    }
+    return result
 }
 
 // MARK: - Part B
 
-private func fixUpdateThroughReorder(line: String, orderedRules: [Pair]) -> Int {
-    let pages = line.components(separatedBy: ",").map { Int($0)! }
-    var printedPages = Set<Int>()
-    var newPages = [Int]()
-    var isDone = false
-//    print(pages)
-    while newPages.count < pages.count {
-        let nextPage = pages.first { page in
-            guard !newPages.contains(page) else { return false }
-            return orderedRules.allSatisfy { $0.last != page || newPages.contains($0.first) || !pages.contains($0.first) }
-        }
-        newPages.append(nextPage!)
-//        print(newPages)
-    }
-    return newPages[newPages.count / 2]
-}
-
 func day5_2025_B() throws -> Int {
     let lines = try FileReader(filename: "day5_2025_input").getGroupedLines()
-    let pairs = try lines[0].map { try parseDictionaryLine($0) }
-    let rules = Set<Pair>(pairs)
-    let wronglyOrdered = lines[1].filter { testUpdate(line: $0, rules: rules) == 0 }
-    let results = wronglyOrdered.map { fixUpdateThroughReorder(line: $0, orderedRules: pairs) }
-//    print(results)
-    return results.reduce(0, +)
+    let freshRanges = lines[0].map {
+        let components = $0.components(separatedBy: "-")
+        return Int(components[0])!...Int(components[1])!
+    }
+    // Too slow
+//    var resultSet = Set<Int>()
+//    freshRanges.enumerated().forEach { (index, element) in
+//        print(index)
+//        resultSet.formUnion(element)
+//    }
+//    return resultSet.count
 
+    // Too slow
+//    var result = 0
+//    freshRanges.enumerated().forEach { (index, range) in
+//        print(index)
+//        result += freshRanges.count
+//        range.forEach { element in
+//            result -= freshRanges.reduce(into: 0) { partialResult, testedRange in
+//                guard testedRange != range else { return }
+//                partialResult += testedRange.contains(element) ? 1 : 0
+//            }
+//        }
+//    }
+//    return result
+
+    // We need to merge together ranges when possible
+    var rangesIteration = freshRanges.sorted { (leftRange: ClosedRange<Int>, rightRange: ClosedRange<Int>) in
+        if leftRange.lowerBound < rightRange.lowerBound {
+            return true
+        } else if leftRange.lowerBound == rightRange.lowerBound {
+            return leftRange.upperBound < rightRange.upperBound
+        }
+        return false
+    }
+    var nextRangesIteration = [ClosedRange<Int>]()
+    var keepReducing = true
+    while keepReducing {
+        var usedRanges: [ClosedRange<Int>: Bool] = rangesIteration.reduce(into: [:]) { partialResult, range in
+            partialResult[range] = false
+        }
+        rangesIteration.forEach { leftRange in
+            guard let rangeToMerge = (rangesIteration.first { rightRange in
+                guard leftRange != rightRange else { return false }
+                guard usedRanges[rightRange] == false else { return false}
+                return leftRange.contains(rightRange.lowerBound) && rightRange.upperBound > leftRange.upperBound
+            }) else {
+                if usedRanges[leftRange] == false {
+                    nextRangesIteration.append(leftRange)
+                    usedRanges[leftRange] = true
+                }
+                return
+            }
+            let mergedRanges = leftRange.lowerBound...rangeToMerge.upperBound
+            nextRangesIteration.append(mergedRanges)
+            usedRanges[leftRange] = true
+            usedRanges[rangeToMerge] = true
+        }
+        keepReducing = nextRangesIteration.count != rangesIteration.count
+        rangesIteration = nextRangesIteration
+            .filter { leftRange in
+                if let foundRange = (nextRangesIteration.first(where: { rightRange in
+                    rightRange.contains(leftRange)
+                })) {
+                    guard foundRange != leftRange else { return true }
+                    return false
+                }
+                return true
+            }
+            .sorted { (leftRange: ClosedRange<Int>, rightRange: ClosedRange<Int>) in
+            if leftRange.lowerBound < rightRange.lowerBound {
+                return true
+            } else if leftRange.lowerBound == rightRange.lowerBound {
+                return leftRange.upperBound < rightRange.upperBound
+            }
+            return false
+        }
+
+        nextRangesIteration = [ClosedRange<Int>]()
+    }
+    print(rangesIteration)
+    return rangesIteration.reduce(into: 0) { partialResult, range in
+        partialResult += range.count
+    }
 }
+
+// 318990863109998 - too Low
+// 335107036446171 - too Low
+// 335427609353607 - Incorrect
+// 355869632640118 - too High
